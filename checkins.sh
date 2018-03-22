aa77dda56f962e35d197bb7673210abe84c5f17f
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-03-22 11:15:56 +0530

    Change API url

--- checkins.sh
+++ checkins.sh
@@ -2,7 +2,7 @@
 DATETOUSE="$(TZ=Asia/Kolkata date "+%A. %B %d, %Y")"
 SUBJECT="Checkins! $DATETOUSE"
 CONTENT="Welcome to [checkins](https://raw.githubusercontent.com/punchagan/namma-bots/master/checkins.txt)!"
-curl https://api.zulip.com/v1/messages \
+curl https://namma-loafers.zulipchat.com/api/v1/messages \
     -u $ZULIP_BOT_EMAIL_CHECKINS:$ZULIP_BOT_KEY_CHECKINS \
     -d "type=stream" \
     -d "to=checkins" \

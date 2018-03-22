7e69c20327b2b5b90e955c691bf1deb5f9761aef
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-03-22 10:54:39 +0530

    Change link to checkins.txt

--- checkins.sh
+++ checkins.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DATETOUSE="$(TZ=Asia/Kolkata date "+%A. %B %d, %Y")"
 SUBJECT="Checkins! $DATETOUSE"
-CONTENT="Welcome to [checkins](checkins.txt)!"
+CONTENT="Welcome to [checkins](https://raw.githubusercontent.com/punchagan/namma-bots/master/checkins.txt)!"
 curl https://api.zulip.com/v1/messages \
     -u $ZULIP_BOT_EMAIL_CHECKINS:$ZULIP_BOT_KEY_CHECKINS \
     -d "type=stream" \

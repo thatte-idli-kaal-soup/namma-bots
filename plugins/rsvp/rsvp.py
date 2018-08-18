f7142958ec67b7a633b0302fa98f3f79776487fa
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-08-18 23:47:29 +0530

    rsvp: Use normal APIs instead of special /zulip api

--- plugins/rsvp/rsvp.py
+++ plugins/rsvp/rsvp.py
@@ -1,34 +1,68 @@
-import logging
+from datetime import datetime, timedelta
 import os
+import re
 
 from errbot import BotPlugin, botcmd
 import requests
+
+# FIXME: Worth making this a config?
+BASE_URL = 'https://rsvp.thatteidlikaalsoup.team/api'
 
 
 class RSVP(BotPlugin):
     """Plugin to enable RSVPing from Zulip."""
 
+    @staticmethod
+    def get_event_id(msg):
+        headers = {
+            'Authorization': 'token {}'.format(os.environ['RSVP_TOKEN'])
+        }
+        match = re.match(
+            '(?P<name>.*) - (?P<date>\d{4}-\d{2}-\d{2} \d{2}:\d{2})',
+            msg.to.subject,
+        )
+        if not match:
+            return 'No date found from topic'
+
+        name, date = match.groups()
+        start_date = datetime.strptime(date, '%Y-%m-%d %H:%M')
+        end_date = start_date + timedelta(days=1)
+        url = '{}/events/?start={:%Y-%m-%d}&end={:%Y-%m-%d}'.format(
+            BASE_URL, start_date, end_date
+        )
+        events = requests.get(url, headers=headers).json()
+        events = [event for event in events if event['name'] == name]
+        if len(events) == 1:
+            return events[0]['_id']['$oid']
+
+    @staticmethod
+    def do_rsvp(event_id, email):
+        headers = {
+            'Authorization': 'token {}'.format(os.environ['RSVP_TOKEN'])
+        }
+        url = '{}/rsvps/{}'.format(BASE_URL, event_id)
+        response = requests.post(
+            url, json={'user': email}, headers=headers
+        ).json()
+        return response
+
     @botcmd
     def rsvp(self, msg, args):
         """RSVP to the app"""
         sender_email = args.strip().split()[0] if args else msg.frm.id
-        data = {
-            'token': os.environ['RSVP_TOKEN'],
-            'message': {
-                'subject': msg.to.subject,
-                'display_recipient': msg.to.id,
-                'content': msg.body,
-                'sender_email': sender_email,
-            },
-        }
-        # FIXME: Worth making this a config?
-        url = 'https://rsvp.thatteidlikaalsoup.team/zulip'
         try:
-            response = requests.post(url, json=data)
-        except Exception as e:
-            status = 'Failure'
-            message = str(e)
-        else:
-            status = 'Failure' if response.status_code > 201 else 'Success'
-            message = response.json()['response_string']
-        return '{}: {}'.format(status, message)
+            event_id = self.get_event_id(msg)
+        except Exception:
+            event_id = None
+        if not event_id:
+            return 'Could not find event'
+
+        try:
+            response = self.do_rsvp(event_id, sender_email)
+        except Exception:
+            return 'Failed to RSVP'
+
+        rsvp_id = response.get('_id', {}).get('$oid', '')
+        return "Successfully RSVP'd" if rsvp_id else response.get(
+            'error', 'Failed to RSVP'
+        )

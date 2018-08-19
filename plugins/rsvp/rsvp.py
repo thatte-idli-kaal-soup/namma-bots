88327a8e42b87a3fadcee71dab6e8aa6b1522956
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-08-19 10:47:03 +0530

    rsvp: Add a command to list the RSVPs for an event

--- plugins/rsvp/rsvp.py
+++ plugins/rsvp/rsvp.py
@@ -21,9 +21,6 @@ class RSVP(BotPlugin):
             '(?P<name>.*) - (?P<date>\d{4}-\d{2}-\d{2} \d{2}:\d{2})',
             msg.to.subject,
         )
-        if not match:
-            return 'No date found from topic'
-
         name, date = match.groups()
         start_date = datetime.strptime(date, '%Y-%m-%d %H:%M')
         end_date = start_date + timedelta(days=1)
@@ -46,6 +43,15 @@ class RSVP(BotPlugin):
         ).json()
         return response
 
+    @staticmethod
+    def get_event(event_id):
+        headers = {
+            'Authorization': 'token {}'.format(os.environ['RSVP_TOKEN'])
+        }
+        url = '{}/rsvps/{}'.format(BASE_URL, event_id)
+        response = requests.get(url, headers=headers).json()
+        return response
+
     @botcmd
     def rsvp(self, msg, args):
         """RSVP to the app"""
@@ -66,3 +72,28 @@ class RSVP(BotPlugin):
         return "Successfully RSVP'd" if rsvp_id else response.get(
             'error', 'Failed to RSVP'
         )
+
+    @botcmd
+    def rsvp_list(self, msg, args):
+        """List of RSVPs for an event"""
+        try:
+            event_id = self.get_event_id(msg)
+        except Exception as e:
+            event_id = None
+        if not event_id:
+            return 'Could not find event'
+
+        event = self.get_event(event_id)
+        names = [
+            rsvp['user']['nick'] or rsvp['user']['name']
+            for rsvp in event['rsvps']
+            if not rsvp['cancelled']
+        ]
+        rsvp_list = '\n'.join(
+            [
+                '{}. {}'.format(i, name)
+                for (i, name) in enumerate(names, start=1)
+            ]
+        )
+        content = 'All RSVPs:\n\n{}'.format(rsvp_list)
+        return content if names else 'No RSVPs'

db0e8ced12e00920cd08c2ecdc2a776d23196f3b
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-08-29 10:32:35 +0530

    welcome: Allow addressing a user

--- plugins/welcome/welcome.py
+++ plugins/welcome/welcome.py
@@ -1,6 +1,6 @@
 from errbot import BotPlugin, botcmd
 
-MESSAGE = """You should [read this message](https://namma-loafers.zulipchat.com/#narrow/stream/115322-zulip/subject/Keybindings.2FShortcuts/near/131101688) - some shortcuts + a picture that sort of describes the UI - to start feeling comfortable here.
+MESSAGE = """{}, you should [read this message](https://namma-loafers.zulipchat.com/#narrow/stream/115322-zulip/subject/Keybindings.2FShortcuts/near/131101688) - some shortcuts + a picture that sort of describes the UI - to start feeling comfortable here.
 
 If you are on a desktop, set your zoom level to 120% or so, to make the UI less overwhelming (`Ctrl+Shift++`)
 """
@@ -12,4 +12,4 @@ class Welcome(BotPlugin):
     @botcmd
     def welcome(self, msg, args):
         """Welcome new users"""
-        return MESSAGE
+        return MESSAGE.format(args)

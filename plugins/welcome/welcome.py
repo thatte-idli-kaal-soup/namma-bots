8bb632cfb20e33bb2c641d7a0f66842ce4ddea04
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-08-31 17:18:11 +0530

    welcome: Improve the welcome message

--- plugins/welcome/welcome.py
+++ plugins/welcome/welcome.py
@@ -1,8 +1,24 @@
 from errbot import BotPlugin, botcmd
 
-MESSAGE = """{}, you should [read this message](https://namma-loafers.zulipchat.com/#narrow/stream/115322-zulip/subject/Keybindings.2FShortcuts/near/131101688) - some shortcuts + a picture that sort of describes the UI - to start feeling comfortable here.
+MESSAGE = """{}, welcome to :zulip:!
+
+Check out the tour [here](https://zulipchat.com/hello/) to get a quick overview of Zulip's streams and topics.
+
+[This comic](https://pbs.twimg.com/media/DbCNzaPX4AATH1A.jpg) gives the same overview, if you prefer hand drawn comics!
 
 If you are on a desktop, set your zoom level to 120% or so, to make the UI less overwhelming (`Ctrl+Shift++`)
+
+**A few keyboard shortcuts**
+
+- `Ctrl + Enter` sends a new message. (can be changed by clicking a checkbox below compose box)
+- `Enter` just inserts a new line - to send longer messages.
+- `?` will show a list of keyboard shortcuts - quite useful.
+- `n` will take you to the next unread topic.
+- `j` and `k` will scroll through messages within the current narrow/view.
+- `p` will take you to next unread private message.
+-  :left:  key lets you edit the last sent message.
+- Hitting the `+` key when a message is selected (blue frame appears around it) adds a :+1: reaction to it. Hit `:` to add other reactions.
+
 """
 
 
